7952fe25cb72b75c87ef18029a9c968952acb869
humangutzmer672 <humangutzmer672@gmail.com>
2025-03-18 06:21:28 +0000

    Add 13.js

---
+++ src/13.js
@@ -0,0 +1,8 @@
+// Generate a random number between 1 and 10
+const randomNumber = Math.floor(Math.random() * 10) + 1;
+
+// Use the random number to create a new string with a length equal to the number
+const randomString = 'a'.repeat(randomNumber);
+
+// Return the new string
+return randomString;
